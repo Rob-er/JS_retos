bed27fed057ee79738675ddc02a8defc5f25197f
Erik <mqer01@gmail.com>
2021-01-26 21:55:35 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,45 @@
+/*let edad = 23;
+let edad_minima = 21;
+let ciudad =  prompt("Ciudad") //"CDMX"
+
+if (edad >= edad_minima && ciudad === "CDMX") {
+    console.log("si puede votar");
+} else{
+    console.log("No puede votar");
+}*/
+
+/*let nombre= prompt("nombre autor");
+  switch(nombre) {
+     case "Sartre":
+         console.log("La nausea, La bola de Cebo, El exitencialismo es humanismo");
+         break;
+     case "Vargas Llosa":
+         console.log("La Ciudad y los peros, las 4 esquinas");
+         break;
+     case "Garcia Marquez":
+         console.log("El coronel no tiene quien le escriba, 100 años de soledad");
+         break;
+     default: 
+     console.log("No esta el autor");
+     break;
+ } */
+
+ /* let inicio = Number(prompt("Numero de inicio"));
+ let fin = Number(prompt("Numero de Fin"));
+ let residual= Number(prompt("Dividido entre"));
+
+ for(inicio; inicio >= fin; inicio--){
+     if(inicio % residual === 0){
+         console.log(inicio);
+     }
+ } */
+
+ /* let inicio = Number(prompt("Numero"));
+ while(inicio > 0){
+     if(inicio % 2 !== 0){
+         console.log(inicio);
+     }
+     inicio--
+ }
+ */
+
