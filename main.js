e3f6f3e384f49a2a7d4c7ad8d3d16b858cf152e3
Erik <mqer01@gmail.com>
2021-02-16 17:13:45 -0600

    ejercicio2

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ function mayor (id1, id2){
     console.log(`El valor mas grande es`, Math.max(numero1, numero2));
 }
 
+// Ejercicio 2
+/* Desarrolle un algoritmo que permita leer tres valores y almacenarlos en las variables A, B y C
+respectivamente. El algoritmo debe imprimir cual es el mayor y cual es el menor. 
+Recuerde constatar que los tres valores introducidos por el teclado sean valores distintos. 
+Presente un mensaje de alerta en caso de que se detecte la introducción de valores iguales */
+
+function mayor_menos(id1, id2, id3){
+    const A = Number(document.getElementById(id1).value);
+    const B = Number(document.getElementById(id2).value);
+    const C = Number(document.getElementById(id3).value);
+
+    const mayor = Math.max(A,B,C);
+    const menor = Math.min(A,B,C);
+
+    if(A === B || A === C || B === C){
+        alert("Alguno de los valores es repetido");
+    } else{
+        document.write(`El mayor es ${mayor} y el menor es ${menor}`);
+    }
+
+}
