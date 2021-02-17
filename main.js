919253b1a75d3f889aa2971c39a8d0a212f37def
Erik <mqer01@gmail.com>
2021-02-16 22:00:05 -0600

    ejercicio con listener

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 
-function buton (id){
+/* function buton (id){
     const boton = document.getElementById(id);
-    console.log(boton);
+    const padre = boton.parentNode
+    let aleatorio= Math.floor(Math.random() * 12345678);
+    let hijo = document.createElement("h1");
+    hijo.innerHTML= "Hola padre";
+    padre.appendChild(hijo);
+
+    padre.style.backgroundColor = "#" + aleatorio;
+} */
+
+// con listener 
+
+function boton (id){
+    const buton_2 = document.getElementById(id);
+    buton_2.addEventListener("click", () => {
+        let padre = buton_2.parentNode;
+        let aleatorio= Math.floor(Math.random() * 12345678);
+        let hijo = document.createElement("h1");
+        hijo.innerHTML= "Hola padre";
+        padre.appendChild(hijo);
+        padre.style.backgroundColor = "#" + aleatorio;
+    } )
 }
+
+document.querySelector('#alone').addEventListener("click", () => {
+    document.body.innerHTML = "";
+})
+
+
+
+
+
+
+
