8da0d8394127ca750b3e9577a2fb3d5304527ed2
Erik <mqer01@gmail.com>
2021-02-17 21:01:19 -0600

    listener funcionando sin funcion

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-/* function buton (id){
+ /* function buton (id){
     const boton = document.getElementById(id);
     const padre = boton.parentNode
     let aleatorio= Math.floor(Math.random() * 12345678);
@@ -8,21 +8,28 @@
     padre.appendChild(hijo);
 
     padre.style.backgroundColor = "#" + aleatorio;
-} */
+}  */
 
 // con listener 
-
-function boton (id){
-    const buton_2 = document.getElementById(id);
-    buton_2.addEventListener("click", () => {
-        let padre = buton_2.parentNode;
+    const list_id = [];
+    const list_botones = [];
+    // Obtengo todos los nodos con el selector CSS Button
+    const botones = document.querySelectorAll("button");
+    // Recorro con un ForEach cada Button y lo agrego con push a la lista list_id su ID
+    let ids = botones.forEach(el => list_id.push(el.id));
+   /*  Recorro con un forEach list_id y obtengo un elemento por id pasando como parametro cada id de 
+       list_id y lo agrego con un push a list_botones*/
+    let btns = list_id.forEach(el => list_botones.push(document.getElementById(el)));
+    // Rocorro lsit botones con un forEach y agrego a cada elemto un Event Handler al dar dar click se ejeucuta 
+    list_botones.forEach(el => el.addEventListener("click", ()=>{
+        let padre = el.parentNode;
         let aleatorio= Math.floor(Math.random() * 12345678);
         let hijo = document.createElement("h1");
         hijo.innerHTML= "Hola padre";
         padre.appendChild(hijo);
         padre.style.backgroundColor = "#" + aleatorio;
-    } )
-}
+    }))
+        
 
 document.querySelector('#alone').addEventListener("click", () => {
     document.body.innerHTML = "";
