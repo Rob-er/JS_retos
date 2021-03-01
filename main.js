e9007d45e1a53b1b99a18d4dfa4b43211d82db46
Erik <mqer01@gmail.com>
2021-03-01 00:28:09 -0600

    mas ejercicios

--- main.js
+++ main.js
@@ -98,11 +98,180 @@ cilindro(5,20); */
  Se asume que la nota está comprendida entre 1 y 20 */
 
 
-function calificaciones (nota){
-   nota === 19 || 20 ? console.log("A") : nota === 16 || 17 || 18 ?  console.log("B") : nota === 13 || 14 || 15 ? console.log("C"): console.log("D");
+/* function calificaciones (nota){
+   nota === 19 || nota === 20 ? console.log("A") : nota === 16 || nota === 17 || nota === 18 ?  console.log("B") : nota === 13 || nota === 14 || nota === 15 ? console.log("C"):  nota === 12 || nota === 11 || nota === 10 ? console.log("D") : console.log("E");
 }
 
-calificaciones(14);
+calificaciones(19); */
 
+// a. Realiza el mismo algoritmo utilizando Mientras (While); también hazlo utilizando En Caso (Case)
+
+/* function cali_while (nota){
+    while(nota === 19 || nota === 20){
+          nota = "A"
+        console.log(nota);
+    } while (nota === 18|| nota === 17 || nota === 16){
+        nota ="B"
+        console.log(nota);
+    }
+}
+cali_while(16) */
+
+/* function cali_case (nota) {
+    switch (nota !== null){
+        case (nota=== 19 || nota === 20):
+            nota = "A"
+            console.log(nota);
+            break;
+
+        case (nota=== 18 || nota === 17 || nota === 16):
+            nota = "B"
+            console.log(nota);
+            break;    
+        
+        default:
+            console.log("ese es nulo");
+            break;
+    }
+}
+
+cali_case(12); */
+
+//Ejercicio 8
+// Desarrolle un algoritmo que permita leer dos números y ordenarlos de menor a mayor, si es el caso.
+/* const mayor_menor = function (nu1, nu2){
+    nu1 > nu2 ? console.log(`De menor a mayor es: ${nu2}, ${nu1}`) : nu1 < nu2 ? console.log(`De menor a mayor es: ${nu1}, ${nu2}`) : console.log(`${nu1} es igual que ${nu2}`)
+}
+
+mayor_menor(2,2); */
+
+// Ejercicio 9 
+//  Desarrolle un algoritmo que permita leer un valor entero positivo N y determinar si es primo o no
+
+/* function entero (valor){
+    valor > 0 && valor % 1 === 0 ? valor % 2 !== 0  ? alert(`${valor} es primo entero positivo`) : alert(`${valor} no es primo, pero si es entero positivo`) : alert(`${valor} no es un numero entero positivo`)
+}
+ entero(13) */
+
+// b. Realice un algoritmo que determine los veinte primeros números, ¿Cuáles son múltiplos de 2?.
+
+/* function los20 (inicio = 0){
+    let contador = 0
+    do{
+        inicio % 2 === 0 ? document.write(inicio + "<br>") : "" 
+        console.log(inicio++), contador++
+    } while (contador <= 20)
+}
+
+los20() */
+
+// c. Realice un algoritmo que determine cuantos minutos hay en 5 horas
+
+/* function minutos(hora = 24){
+    console.log(`en ${hora} hr(s) hay ${hora *60} minutos`)
+}
+minutos(40) */
+
+//Ejercicio 10
+/* Realice un algoritmo que calcule el monto a pagar por el servicio de estacionamiento, 
+teniendo en cuenta que por la primera hora de estadía se tiene una tarifa de 1000 bolívares
+y las restantes tienen un costo de 600 bolívares. Se tiene como datos: hora de entrada,
+hora de salida (formato militar), iniciada una hora se contabiliza como hora total. */
+
+/* function apagarEstacionamiento(hrsEntrada = 13, hrSalida = 14){
+    let total = 0;
+    do {
+
+       Math.floor(hrSalida) - Math.floor(hrsEntrada)> 1 ? total += 600 : total += 1000;
+       Math.floor(hrsEntrada++);
+
+    } while (Math.floor(hrsEntrada) < Math.floor(hrSalida))
+    console.log(`El total a pagar por estacionamiento es ${total}`)
+}
+apagarEstacionamiento() */
+
+/* Realice un algoritmo que determine el pago a realizar por la entrada a un espectáculo:
+a) donde se pueden comprar sólo hasta cuatro entradas, 
+b) donde al costo de dos entradas se les descuenta el 10%, al de tres entrada el 15% 
+   y a la compra de cuatro tickets se le descuenta el 20 %. */
+
+/* function pagoEntrada(precio = 5, boletos = 1){
+    let total = precio * boletos;
+    if (boletos <= 4){
+
+        boletos === 1 ? console.log(`El total apagar por ${boletos} es ${total}`) : boletos === 2 ? console.log(`El total apagar por ${boletos} es ${total * (1- .10)}`) :  boletos === 3 ? console.log(`El total a pagar por ${boletos} es ${total * (1 - .15)}`) : console.log(`El total a pagar por ${boletos} es ${total * (1 - .20)}`)
+    } else {
+        console.log(`${boletos} es una cantidad excesiva, el limite es 4`)
+    }
+}
+
+pagoEntrada(10, 3) */
+
+// Ejercicio 11
+/* Realice un algoritmo que a partir de proporcionarle la velocidad de un automóvil, expresada en
+kilómetros por hora, proporcione la velocidad en metros por segundo. */
+
+/* function de_Km_a_mts(velocidad){
+    console.log(`La velocidad es de ${(velocidad * 1000) / 3600} metros por segundo`)
+}
+
+de_Km_a_mts(9) */
+
+// a. Desarrolle un algoritmo que lea la velocidad en metros por segundo y la convierta a kilómetros por
+//    hora. 
+
+/* console.log((2.5 * 3600) / 1000) */
+
+// Ejercicio 12
+// Desarrolle un algoritmo que permita calcular Promedio de Notas; finaliza cuando N = 0.
+
+/* function promNotas(...notas){
+   let total = notas.reduce((acc, actu) => {
+        return acc += actu
+    }, 0)
+    console.log(`Su promedio es ${total / notas.length}`)
+}
+
+promNotas(2) */
+
+// Ejercicio 13
+/* Desarrolle un algoritmo para la empresa Constructora Tecnovivir Casas C.A., 
+que le permita calcular e imprimir la nómina para su cancelación a un total de 50 obreros
+calificados a quienes debe cancelar por horas trabajadas. La hora trabajada se pautó en 30.000 Bolívares. */
+ 
+class Obrero {
+    constructor (nombre, hrs_Trabajadas){
+        this.nombre = nombre
+        this.hrs_Trabajadas = hrs_Trabajadas
+    }
+    soy(){
+        document.write(`Hola soy el ${this.nombre} y trabaje ${this.hrs_Trabajadas} hrs <br>`)
+    }
+}
+
+class Nomina{
+    constructor(lista){
+        this.lista = [];
+    }
+
+    calculo_nomina(nombre, hrs){
+        this.lista.push(`${nombre} debe recibir ${hrs * 30000}`)
+    }
+}
+
+function nomina (){
+    const total = new Nomina;
+    let hrs = 0
+    let numero = 1
+    do {
+        let obrero = new Obrero(`${numero} obrero`, hrs += 1)
+        obrero.soy()
+        total.calculo_nomina(obrero.nombre, obrero.hrs_Trabajadas)
+        numero++
+    }while(numero <= 50)
+    console.table(total.lista)
+}
+
+nomina()
 
 
